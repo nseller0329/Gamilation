0b18701b1c351345bfe13bfdad10f138419025b6
nseller0329 <31457103+nseller0329@users.noreply.github.com>
2021-03-01 18:30:02 -0800

    logic to render image based on genre

---
+++ src/common/images.js
@@ -0,0 +1,10 @@
+import rpg from '../assets/rpg.png';
+import shooter from '../assets/shooter.png';
+import adventure from '../assets/adventure.png';
+
+var images = {
+    rpg: rpg,
+    shooter: shooter,
+    adventure: adventure
+};
+export default images;
